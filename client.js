f0626e70a78d3bd350c879e5bd1bbfd46f199701
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-26 12:24:55 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,18 @@
+console.log('this is showing up');
+
+let card = document.getElementsByClassName("card");
+console.log(card)
+
+let cards = [];
+function newGame () {
+  let card = document.getElementsByClassName("card");
+
+  // for (var i = 16; i > cards.length; i++) {
+  //   cards.push(card);
+  // }
+}
+
+newGame();
+
+console.log(cards)
+

958cd80334405c539817f3426815085286eaede0
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-27 15:44:41 -0500

    cards finding matches, function running for all matches

--- client.js
+++ client.js
@@ -5,41 +5,52 @@ for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', selectedCard)
 }
 
-console.log('cards', cards)
-
-let eventTracker = [];
+// console.log('cards', cards)
 
 function selectedCard(event) {
   console.log('card clicked', event.target.attributes.type.value);
-  eventTracker.push(event);
-  console.log('event tracker', eventTracker)
   checkMatch(event);
 }
 
 let firstCard = null
 let secondCard = null;
 
+let currentScore = 0;
+let attempts = 0;
+
+document.getElementById("currentScore").innerHTML = 'Current Score: ' + currentScore;
+document.getElementById("attempts").innerHTML = 'Total Attempts: ' + attempts;
+
 function checkMatch(cardGuess) {
-  var trackingArray = [];
 
   if (firstCard === null) {
     firstCard = cardGuess;
     firstCard.target.classList.toggle('green');
-    console.log('first card', firstCard)
+    // console.log('first card', firstCard)
   }
   else if (secondCard === null) {
     secondCard = cardGuess;
-    console.log('second card', secondCard)
+    // console.log('second card', secondCard)
     if (firstCard.target.attributes.type.value === secondCard.target.attributes.type.value ) {
       console.log('cards match!')
-      secondCard.target.classList.toggle('green', 'matched');
+      firstCard.target.classList.toggle('matched');
+      secondCard.target.classList.toggle('green');
+      secondCard.target.classList.toggle('matched');
+      currentScore = currentScore + 1;
+      attempts = attempts + 1;
       firstCard = null;
       secondCard = null;
-      // clearArray();
+      document.getElementById("currentScore").innerHTML = 'Score: ' + currentScore;
+      document.getElementById("attempts").innerHTML = 'Total Attempts: ' + attempts;
+      if (currentScore === 10) {
+        youWin();
+      }
+
     }
     else {
       console.log('cards do not match')
-      // console.log('eventTracker class list', eventTracker[0].target.classList)
+      attempts = attempts + 1;
+      document.getElementById("attempts").innerHTML = 'Total Attempts: ' + attempts;
       firstCard.target.classList.toggle('green', 'orange')
       secondCard.target.classList.toggle('orange');
       setTimeout(clearGuess, 1000)
@@ -62,15 +73,10 @@ function clearGuess() {
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
@@ -85,6 +91,10 @@ function startGame() {
     newCard = cards[i];
     document.getElementById('cardDeck').appendChild(newCard);
   }
+}
+
+function youWin() {
+  console.log('game is over')
 }
 
 startGame();

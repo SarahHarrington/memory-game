6167b1f7e5595584c4c5cc0ceb2a8a7dccb14902
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-26 22:50:23 -0500

    working on function to check matches

--- client.js
+++ client.js
@@ -1,49 +1,69 @@
-console.log('this is showing up');
+let card = document.getElementsByClassName('card');
+let cards = [...card];
 
-var cards = [
-  { 
-    name: 'apple',
-    image: 'images/apple.png'
-  }, 
-  { 
-    name: 'apricot',
-    image: 'images/apricot.png'
-  }, 
-  { 
-    name: 'banana',
-    image: 'images/banana.png'
-  }, 
-  { 
-    name: 'cherry',
-    image: 'images/cherry.png'
-  },
-  { 
-    name: 'grapes',
-    image: 'images/apple.png'
-  },  
-  { 
-    name: 'lemon',
-    image: 'images/lemon.png'
-  }, 
-  { 
-    name: 'orange',
-    image: 'images/orange.png'
-  }, 
-  { 
-    name: 'strawberries',
-    image: 'images/strawberries.png'
-  }, 
-]
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', selectedCard)
+}
 
-// function newGame () {
-//   var card;
-//   for (var i = 0; i < cards.length; i++) {
-//     card = cards[i].image;
-//     document.getElementById('cardDeck').appendChild()
-//   }
-// }
+console.log('cards', cards)
 
-var card = document.getElementsByClassName('card');
+function selectedCard(event) {
+  console.log('card clicked', event.target.attributes.type.value);
+  event.target.classList.add('green');
 
-console.log('card', card)
+  checkMatch(event);
+}
 
+let firstCard = null
+let secondCard = null;
+function checkMatch(cardGuess) {
+
+  if (firstCard === null) {
+    firstCard = cardGuess;
+    console.log('first card', firstCard)
+  }
+  else if (secondCard === null) {
+    secondCard = cardGuess;
+    console.log('second card', secondCard)
+    if (firstCard.target.attributes.type.value === secondCard.target.attributes.type.value ) {
+      secondCard.classList.add('green');
+    }
+    else {
+      console.log('cards do not match')
+      firstCard.classList.remove('green');
+      firstCard = null;
+      secondCard = null;
+    }
+  }
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function startGame() {
+  shuffle(cards);
+  var newCard;
+  for (var i = 0; i < cards.length; i++) {
+    newCard = cards[i];
+    document.getElementById('cardDeck').appendChild(newCard);
+  }
+}
+
+startGame();
+
+console.log('Cards', cards);
